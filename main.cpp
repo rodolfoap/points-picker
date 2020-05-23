429f9a14b39fbc57b8c51a1dee2e1ea6b13c3156
RodolfoAP <rodolfo.alcazar@gtd.eu>
2020-05-23 14:45:31 +0200

    Changes 2020/05/23-14:45:31
    Change-Id: I4ca8b2d06e6f9c9584380cea7aae0708ffd92d4f

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
 	if(argc<3) { std::cerr<<"Usage: "<<argv[0]<<" [IMAGE_FILE POINTS_FILE MUSTACHE_TEMPLATE]" <<std::endl; return 0; }
 	sf::Event event;
 	Form form(argv[2], argv[3]);
-	Window window(1920, 1080, "PPicker", argv[1], form.cSize());
+	Window window(1280, 720, "PPicker", argv[1], form.cSize());
 
 	// Loop
 	form.message(window);
